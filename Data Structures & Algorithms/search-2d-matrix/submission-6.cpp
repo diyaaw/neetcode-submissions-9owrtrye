a7c49394258432b33b01b7674762c7502b2ed109
diyaaw <diya.wadhwa24@gmail.com>
2026-05-07 16:10:07 +0530

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+// bool binarysearch(vector<int>& row, int target){
+// int n=row.size();
+
+// int start=0;
+// int end=n-1;
+// while(start<=end){
+//     int mid=start+(end-start)/2;
+//     if(target==row[mid]) return true;
+//     else if(row[mid]<target){
+//         start=mid+1;
+//     }
+//     else end=mid-1;
+// }
+// return false;
+// }
+   bool searchMatrix(vector<vector<int>>& matrix, int target) {
+  int n=matrix.size();
+         int m=matrix[0].size();
+
+//         for(int i=0;i<n;i++){
+//             if(target>=matrix[i][0] && target<=matrix[i][m-1]){
+//                 return binarysearch(matrix[i],target);
+//             }
+//         }
+//         return false;
+
+
+     int start=0;
+     int end=n*m-1;
+     while(start<=end){
+        int mid=start+(end-start)/2;
+        int row=mid/m;
+        int col=mid%m;
+
+        if(matrix[row][col]==target) return true;
+        else if(matrix[row][col]<target) start=mid+1;
+        else end=mid-1;
+     }
+     return false;
+   }
+ };
